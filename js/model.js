af31fc02a01b76777e94284c3c70a09020db76ab
kasparernits <kaspar.ernits@gmail.com>
2018-02-28 22:33:46 +0100

    Minor changes

--- js/model.js
+++ js/model.js
@@ -1,49 +1,46 @@
 window.onload = function () {
 
-    var center = { x: 350, y: 350 };
+    const center = { x: 350, y: 350 };
 
-    class spokeHole {
-        constructor(side, id, x, y){
-            this.side = side;
+    class hole {
+        constructor(id, x, y){
             this.id = id;
             this.x = x;
             this.y = y;
         }
     }
 
-    class hub {
-        constructor(name,flangeWidth,numSpokeHoles){
+    class circle {
+        constructor(name, diameter, width, numOfHoles){
             this.name = name;
-            this.flangeWidth = flangeWidth;
-            this.numSpokeHoles = numSpokeHoles;
-            this.spokeHoles = [];
-            this.addSpokeHole = function (newObject) {
-                this.spokeHoles.push(newObject);
+            this.diameter = diameter;
+            this.width = width;
+            this.numOfHoles = numOfHoles;
+            this.holes = [];
+            this.addHole = function (newObject) {
+                this.holes.push(newObject);
             };
         }
 
-        calcSpokeHoleCoords(thisHole) {
-            var theta = ((Math.PI * 2) / this.numSpokeHoles);
+        calcHoleCoords(thisHole) {
+            var theta = ((Math.PI * 2) / this.numOfHoles);
             var angle = (theta * thisHole);
-            var newX = (this.flangeWidth / 2) * Math.cos(angle) + center.x; // dont like this global variable, to fix later
-            var newY = (this.flangeWidth / 2) * Math.sin(angle) + center.y;
-            return {newX,newY};
+            var newX = (this.diameter / 2) * Math.cos(angle) + center.x;
+            var newY = (this.diameter / 2) * Math.sin(angle) + center.y;
+            return { newX, newY };
         }
 
     }
 
-    var hbt30 = new hub('hbt30',70.6,16);
+    var hbt30 = new circle('hbt30',70.6,10,16);
 
-    for(i=1;i<=hbt30.numSpokeHoles;i++){
-        hbt30.addSpokeHole(new spokeHole('a',i,hbt30.calcSpokeHoleCoords(i).newX, hbt30.calcSpokeHoleCoords(i).newY));
-    }    
+    for(i=0;i<=hbt30.numOfHoles;i++)
+        hbt30.addHole(new hole(i,hbt30.calcHoleCoords(i).newX, hbt30.calcHoleCoords(i).newY));
 
-    console.log(hbt30); 
+    var rim = new circle('8bar super',622,30,32);
 
-    function draw(o, x, y){
-
-        console.log('tbd...');
-    }
+    for(i=0;i<=rim.numOfHoles;i++)
+        rim.addHole(new hole(i,rim.calcHoleCoords(i).newX, rim.calcHoleCoords(i).newY));
 
     // Center point of the rim
     var y = 350;
@@ -71,18 +68,20 @@ window.onload = function () {
     var params = { fullscreen: true }
     var two = new Two(params).appendTo(elem);
 
-    for(i=1;i<=hbt30.numSpokeHoles-1;i++){ // to fix
-        two.makeCircle(hbt30.spokeHoles[i].x, hbt30.spokeHoles[i].y, 3).fill='red';
-    }
+    drawCircle(two, center.x, center.y, hbt30);
 
-    var rim = drawRim(two, center.x, center.y, rimDiameter, rimHeigth);
-    var myHub = drawHub(two, center.x, center.y, hubFlangeWidth);
-    var spoke1 = drawSpoke(two, center.x, getY1(), spokeLength, 'red');
-
-    var spokesRow1 = drawSpokes(two, 750, 320, 1, 16, 'black');
-    var spokesRow2 = drawSpokes(two, 750, 650, 17, 32, 'black');
+    //var spoke1 = drawSpoke(two, center.x, getY1(), spokeLength, 'red');
+    //var spokesRow1 = drawSpokes(two, 750, 320, 1, 16, 'black');
+    //var spokesRow2 = drawSpokes(two, 750, 650, 17, 32, 'black');
 
     two.update();
+
+    function drawCircle(two, x, y, circle) {
+        var hub = two.makeCircle(x, y, circle.diameter);
+        hub.fill = 'white';
+        hub.stroke = 'black';
+        hub.linewidth = circle.width;
+    }
 
     function getY1() {
         return center.y - hubFlangeWidth / 2;
@@ -111,37 +110,6 @@ window.onload = function () {
             });
             startX = startX + 18;
         }
-    }
-
-    function drawRim(two, x, y, rimDiameter, heigth) {
-        var rim = two.makeCircle(x, y, rimDiameter / 2);
-        rim.fill = 'white';
-        rim.stroke = 'black';
-        rim.linewidth = heigth;
-
-        for (var i = 1; i <= rimNumOfHoles; i++) {
-            drawPointsOnCircle(rimDiameter / 2, i, rimNumOfHoles, 2);
-        }
-    }
-
-    function drawHub(two, x, y, flangeWidth) {
-        var hub = two.makeCircle(x, y, flangeWidth / 2)
-        hub.fill = 'white';
-        hub.stroke = 'black';
-        hub.linewidth = 8;
-
-        for (var i = 1; i <= hubNumOfSpokeHoles; i++) {
-            drawPointsOnCircle(hubRadius, i, hubNumOfSpokeHoles, 2);
-        }
-    }
-
-    function drawPointsOnCircle(R, currentPoint, numOfPoints, size) {
-
-        var theta = ((Math.PI * 2) / numOfPoints);
-        var angle = (theta * currentPoint);
-        var newX = R * Math.cos(angle) + center.x;
-        var newY = R * Math.sin(angle) + center.y;
-        two.makeCircle(newX, newY, size);
     }
 
 }
